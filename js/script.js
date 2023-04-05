fa5cd8caf2f168302dc55311eb3e1cf2e1b297ba
Yemi Oduyemi <yhermii@gmail.com>
2023-04-05 03:26:10 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let i=[0];
+let images=[]
+let time=3000;
+
+images[0]="./images/slides/a.jpg"
+images[1]="./images/slides/b.jpg"
+images[2]="./images/slides/c.jpg"
+images[3]="./images/slides/d.jpg"
+images[4]="./images/slides/e.jpg"
+images[5]="./images/slides/f.jpg"
+images[6]="./images/slides/g.jpg"
+
+
+function changeImg(){
+    if(i<images.length){
+        document.slide.src=images[i]
+        i++
+    }else{i=0}
+setTimeout("changeImg()", time);
+}
+window.onload=changeImg;
